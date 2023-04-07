769bf47c4a296963b414c79f282fd73594b5c9da
thoailyo230 <125753220+thoailyo230@users.noreply.github.com>
2023-04-07 08:59:20 +0700

    Update CTDL&GT_binary_search_1.cpp

--- CTDL&GT_binary_search_1.cpp
+++ CTDL&GT_binary_search_1.cpp
@@ -1,4 +1,4 @@
-//Cài đặt tìm kiếm nhị phân
+//Cài đặt tìm kiếm nhị phân, link tham khảo: https://gochocit.com/ky-thuat-lap-trinh/thuat-toan-tim-kiem-nhi-phan-binary-search
 int BinarySearch(int a[],int n,int x)
 {
 	int left, right, mid; left=0; right=n-1;
