4e4bd84115df4b4280d986fafebffcf27c355fc9
thoailyo230 <125753220+thoailyo230@users.noreply.github.com>
2023-04-07 08:50:48 +0700

    Update and rename README.md to CTDL&GT_linear_search_2.cpp

---
+++ CTDL&GT_linear_search_2.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+void Nhap(int a[],int n){
+    for(int i=0; i<n; i++){
+        printf("\nNhap a[%d]=",i);
+        scanf("%d",&a[i]);
+    }
+}
+void Xuat(int a[],int n){
+    for(int i=0; i<n; i++){
+        printf("%d \t",a[i]);
+    }
+}
+int LinearSearch(int a[], int n, int x){
+    for(int i=0; i<n; i++){
+        if(a[i]==x){
+            return i;
+        }
+    }
+    return -1;
+}
+int main(){
+    int a[100];
+    int n;
+    printf("Nhap so phan tu:");
+    scanf("%d",&n);
+    Nhap(a,n);
+    printf("\nMang sau khi nhap la:\n");
+    Xuat(a,n);
+    int x;
+    printf("\nNhap phan tu can tim:");
+    scanf("%d",&x);
+    if(LinearSearch(a,n,x)==-1){
+        printf("Khong tim thay %d trong mang!",x);
+    }else{
+        printf("\nVi tri cua %d trong mang la: %d",x, LinearSearch(a,n,x));
+    }    
+}
