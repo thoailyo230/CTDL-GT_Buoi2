9cb98b2b490b49d61e37b6ab8f9cd76a1d939037
thoailyo230 <125753220+thoailyo230@users.noreply.github.com>
2023-04-07 08:54:22 +0700

    Create CTDL&GT_Tail_Recursion_2.cpp

---
+++ CTDL&GT_Tail_Recursion_2.cpp
@@ -0,0 +1,21 @@
+//ví dụ đệ quy đuôi
+#include <stdio.h>
+ 
+int UCLN(int m, int n){
+  int r;
+  if(m<n) return UCLN(n,m);
+  r = m % n;
+  if(r == 0) return n;
+  else return UCLN(n,r);
+}
+  
+int main(void) {
+  int kq,m,n;
+  printf("Nhap gia tri m: ");
+  scanf("%d", &m);
+  printf("Nhap gia tri n: ");
+  scanf("%d", &n);
+  kq = UCLN(m,n);
+  printf("Ket qua: %d",kq);
+  return 0;  
+}
