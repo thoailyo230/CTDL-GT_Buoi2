38405e4545bbcca501fcd7bdc4eb1d1888b63677
thoailyo230 <125753220+thoailyo230@users.noreply.github.com>
2023-04-07 09:10:10 +0700

    Create CTDL&GT_Mutual_Recursion_1.cpp

---
+++ CTDL&GT_Mutual_Recursion_1.cpp
@@ -0,0 +1,14 @@
+//cài đặt đệ quy tương hỗ, link tham khảo: https://freetuts.net/de-quy-tuong-ho-mutual-recursion-2961.html
+bool isEven(int n);
+bool isOdd(int n);
+ 
+bool isEven(int n){
+  if(n == 0)
+    return true;
+  else
+    return isOdd(n - 1);
+}
+ 
+bool isOdd(int n){
+  return !isEven(n);
+}
