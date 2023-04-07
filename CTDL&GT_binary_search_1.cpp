8cf4f439200058aea317004aa4a45722bf0eab68
thoailyo230 <125753220+thoailyo230@users.noreply.github.com>
2023-04-07 08:53:24 +0700

    Create CTDL&GT_binary_search_1.cpp

---
+++ CTDL&GT_binary_search_1.cpp
@@ -0,0 +1,18 @@
+//Cài đặt tìm kiếm nhị phân
+int BinarySearch(int a[],int n,int x)
+{
+	int left, right, mid; left=0; right=n-1;
+	do{
+		mid=(left+right)/2;
+		if(a[mid]==x){
+			return mid;
+		}
+		else if(a[mid]<x){
+			left=mid+1;
+		}
+		else{
+			right=mid-1;
+		}
+	}while(left<=right);
+	return -1;
+}
