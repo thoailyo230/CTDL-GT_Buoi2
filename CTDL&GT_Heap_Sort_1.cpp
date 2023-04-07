7ce74b567625a812e8d60c60c4543064e99e467b
thoailyo230 <125753220+thoailyo230@users.noreply.github.com>
2023-04-07 09:14:12 +0700

    Create CTDL&GT_Heap_Sort_1.cpp

---
+++ CTDL&GT_Heap_Sort_1.cpp
@@ -0,0 +1,12 @@
+//cài đặt phương pháp sắp xếp cây, link tham khảo: https://vimentor.com/vi/lesson/sap-xep-heap-sort
+
+void max_heap_sort(int A[ ])
+{
+    int heap_size = N;
+    run_maxheap(A);
+    for(int i = N; i>=2 ; i-- )
+    {
+        swap(A[ 1 ], A[ i ]);
+        max_heap(A, 1, --heap_size);
+    }
+}
