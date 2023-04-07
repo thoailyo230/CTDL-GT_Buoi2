cb8cbe76118b1ab833665ca7fcc438a972d7df12
thoailyo230 <125753220+thoailyo230@users.noreply.github.com>
2023-04-07 09:10:52 +0700

    Create CTDL&GT_Linear_Recursion_1.cpp

---
+++ CTDL&GT_Linear_Recursion_1.cpp
@@ -0,0 +1,7 @@
+ //cài đặt đệ quy tuyến tính, link tham khảo: https://freetuts.net/de-quy-tuyen-tinh-2946.html
+ int factorial(int n){
+    if(n==0)
+       return 1;
+   return n*factorial(n-1);
+}
+
